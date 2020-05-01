a0091e31653bf69c97d381460283c030f3c892bc
kain <kainpro@foxmail.com>
2020-05-01 20:28:27 +0800

    项目初始化

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ registerLocaleData(zh);
 
 import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { zh_CN } from 'ng-zorro-antd/i18n';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
@@ -25,6 +27,7 @@ import { environment } from '../environments/environment';
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
+    NzLayoutModule,
     BrowserAnimationsModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
