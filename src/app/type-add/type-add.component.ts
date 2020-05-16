c6d8b0da731b009097d4d528bcf4768730944d39
kain <kainpro@foxmail.com>
2020-05-16 16:49:19 +0800

    初始化管理页面

---
+++ src/app/type-add/type-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-type-add',
+  templateUrl: './type-add.component.html',
+  styleUrls: ['./type-add.component.scss']
+})
+export class TypeAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
