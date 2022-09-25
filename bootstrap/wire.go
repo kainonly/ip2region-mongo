02d79edc83ac7d6417eae436150f0b09362fef16
kain <kainpro@foxmail.com>
2022-09-25 23:24:13 +0800

    UPDATE: 适配为 MySQL

--- bootstrap/wire.go
+++ bootstrap/wire.go
@@ -6,13 +6,11 @@ package bootstrap
 import (
 	"github.com/google/wire"
 	"github.com/kainonly/ip2region-sync/api"
-	"github.com/kainonly/ip2region-sync/common"
 )
 
 func NewAPI() (*api.API, error) {
 	wire.Build(
 		wire.Struct(new(api.API), "*"),
-		wire.Struct(new(common.Inject), "*"),
 		Provides,
 	)
 	return &api.API{}, nil
--- common/common.go
+++ common/common.go
@@ -1,14 +1,5 @@
 package common
 
-import (
-	"gorm.io/gorm"
-)
-
-type Inject struct {
-	Values *Values
-	Db     *gorm.DB
-}
-
 type Values struct {
 	// 监听地址
 	Address string `env:"ADDRESS" envDefault:":9000"`
